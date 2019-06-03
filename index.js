fa0d8cced692782016ee6331ebbc2e1d056e5b2d
Marie-AnneS <marie-anne.seim@gmail.com>
2019-06-03 02:25:01 +0000

    first case

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+/* array.forEach(element => {
+    sumEl += element;
+    console.log(`sumEl : ${sumEl}`)
+  }); */
 
+let sumEl = 0;
+function sumItems(array) {
+  //console.log(Array.isArray(array[0]));
+  if (array.length !== 0) {
+    if (Array.isArray(array[0])) {
+      sumItems(array[0]);
+    }
+    console.log("----------------------------------")
+    console.log(`array[0]: ${array[0]}`);
+    console.log("----------------------------------")
+    sumEl = sumEl + array[0];
+    console.log(`sumEl: ${sumEl}`);
+    sumItems(array.splice(1));
+    console.log("----------------------------------")
+    console.log(`array[0] @2@: ${array[0]}`);
+    console.log("----------------------------------")
+
+    return sumEl;
+  }
+  return;
+}
+console.log(sumItems([[1, 2, [[3], 4]], 5, []])); //[[1, 2, [[3], 4]], 5, []]
 module.exports = sumItems;
